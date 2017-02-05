9694f366cbc0b2fd8cca268436b4cbb065076e95
Xiao Gang <huangzhigang1024@qq.com>
2017-02-05 17:34:29 +0800

    testfile

--- main_test.go
+++ main_test.go
@@ -1,19 +1,23 @@
 package main
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func TestCal(t *testing.T) {
 	cases := []struct {
-		in, want string
+		in1, in2, want string
 	}{
-		{"2+3", "+"},
-		{"2*3", "x"},
-		{"3-2", "-"},
+		{"2+3", "+", "5"},
+		{"2*3", "*", "6"},
+		{"3-2", "-", "1"},
 	}
 	for _, c := range cases {
-		got := Cal(c.in)
-		if got != c.want {
-			t.Errorf("Cal(%q) == %q, want %q", c.in, got, c.want)
+		got := Cal(c.in1, c.in2)
+		want, _ := strconv.Atoi(c.want)
+		if got != want {
+			t.Errorf("Cal(%q,%q) == %q, want %q", c.in1, c.in2, got, want)
 		}
 	}
 }
